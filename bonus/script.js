e5e6622fa3b446769459a1b876c4629d48196f7b
antonella1986 <antonella.leo.mail@gmail.com>
2025-02-12 16:59:08 +0100

    try bonus

--- bonus/script.js
+++ bonus/script.js
@@ -16,13 +16,27 @@ console.log(reversedTeachers);
 
 // 2. Crea un nuovo array chiamato 'longNames' che contenga solo gli insegnanti
 // con un nome di lunghezza maggiore o uguale a 5 caratteri
-const longNames = null;
+
+for (let i = 0; i < teachers.length; i++) {
+  if (teachers.length >= 5) {
+    const longNames = (teachers[i]);
+  }   
+}
+console.log(longNames);
+
+
 
 // 3. Rimuovi 'Ed' dall'array teachers
+teachers.splice(5, 5);
+console.log(teachers);
+/* boh */
+
 
 // 4. Verifica se 'Fabio' è presente nell'array teachers
 // e salva il risultato nella variabile isFabioPresent
-const isFabioPresent = null;
+const isFabioPresent = teachers.indexOf("Fabio");
+console.log(isFabioPresent);
 
-// 5. Unisci tutti gli insegnanti nell'array teachers in una stringa  separata da virgole e salvala nella variabile teachersString
+
+// 5. Unisci tutti gli insegnanti nell'array teachers in una stringa separata da virgole e salvala nella variabile teachersString
 const teachersString = null;
